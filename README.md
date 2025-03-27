7624e3b2206d5d82b36f408529a7257a879b5f8f
Kat <cpnguyen0000@gmail.com>
2025-03-27 14:17:09 -0600

    THE DONUTS HAVE BEEN COOKED

--- README.md
+++ README.md
@@ -15,36 +15,6 @@ With both of them in the same directory you can just run `python3 visuals.py` an
 ## Visual Descriptions
 There are two main visuals that I made using my Pokemon data set: __A Scatterplot__ and a __Double Donut Chart__ (aka a double pie chart). Both of these visualize the same thing. The Scatterpolt... well it plots all of data above their respective types. There's also a very nice ledgend because I'm so nice.
 
-The Double Donut Chart take's it a bit further. The outside circle has all of the types (color codes respectively) along with their persent out of the total set. The inside circle further breaks that down into each "type" that I listed above.
+The Double Donut Chart take's it a bit further. The outside circle has all of the types (color codes respectively) along with their percent out of the total set. The inside circle further breaks that down into each "type" that I listed above. That is also broken down by their percent out of the total.
 
-__*Do note that for my sanity I only did this for Gen 5 and 6 combined. That totals a total of 451 Pokemon in this data sample.*__
-
-
-
-"""
-# More complex double donut visualization
-# Starting by making a single donut to learn - (Preperation for the outside donut)
-plt.figure(figsize=(8, 8))
-labels = df.Type
-sizes = df.Total
-colors = ["#a8a878", "#f08030", "#6890f0", "#78c850", "#f8d030", "#98d8d8", "#c03028", "#a040a0", "#e0c068", "#a890f0", "#f85888", "#a8b820", "#b8a038", "#705898", "#7038f8", "#705848", "#b8b8d0", "#ee99ac"]
-explode = [0] * len(labels)
-textprops = {"fontsize":14, "color":"black"}
-
-# Actually plotting the outside donut
-plt.pie(sizes,
-        explode=explode,
-        pctdistance =0.9,
-        labels=labels,
-        colors=colors,
-        autopct="%.2f%%",
-        shadow=False,
-        textprops =textprops,
-        wedgeprops={"linewidth": 3.0, "edgecolor": "white"})
-
-# Making a circle to go in the center to make it look like a true donut
-center_circle = plt.Circle((0,0),0.65, color="grey", fc="white", linewidth=1.00)
-fig = plt.gcf()
-fig.gca().add_artist(center_circle)
-plt.axis("equal")
-"""
+__*Do note that for my sanity I only did this for Gen 5 and 6 combined. That totals a total of 451 Pokemon in this data sample. Yes it does include multiple forms of a Pokemon if it has one. Yes it is also probably off by a few.*__
--- visuals.py
+++ visuals.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Starts by pulling the data from the csv file
-columns = ["Type", "Total", "Regular", "PseudoLedgendary", "Ledgendary", "Mythical", "Megas"]
+columns = ["Type", "Total", "Regular", "PseudoLegendary", "Legendary", "Mythical", "Megas"]
 df = pd.read_csv("data.csv", usecols=columns)
 
 # Basic scatterplot visualization for the data set
 plt.scatter(df.Type, df.Total, color="red", label="Total")
 plt.scatter(df.Type, df.Regular, color="orange", label="Regular")
-plt.scatter(df.Type, df.PseudoLedgendary, color="green", label="Pseudo Ledgendary")
-plt.scatter(df.Type, df.Ledgendary, color="blue", label="Ledgendary")
+plt.scatter(df.Type, df.PseudoLegendary, color="green", label="Pseudo Legendary")
+plt.scatter(df.Type, df.Legendary, color="blue", label="Ledendary")
 plt.scatter(df.Type, df.Mythical, color="purple", label="Mythical")
 plt.scatter(df.Type, df.Megas, color="pink", label="Mega")
 
@@ -23,33 +23,42 @@ plt.ylabel('Number of Pokemon')
 
 # It's double donut time >:3
 plt.figure(figsize=(8, 8))
+
+# Settings for the outer donut
 labels = df.Type
 sizes = df.Total
 colors = ["#a8a878", "#f08030", "#6890f0", "#78c850", "#f8d030", "#98d8d8", "#c03028", "#a040a0", "#e0c068", "#a890f0", "#f85888", "#a8b820", "#b8a038", "#705898", "#7038f8", "#705848", "#b8b8d0", "#ee99ac"]
-subgroup_labels = ["Regular", "Pseudo Ledgendary", "Ledgendary", "Mythical", "Mega"]
-subgroup_sizes = [df.Regular, df.PseudoLedgendary, df.Ledgendary, df.Mythical, df.Megas]
 
+# Settings for the inner donut
+subgroup_labels = ["Regular", "Pseudo Legendary", "Legendary", "Mythical", "Mega"]
+subgroup_sizes = [df.Regular.sum(), df.PseudoLegendary.sum(), df.Legendary.sum(), df.Mythical.sum(), df.Megas.sum()]
+subgroup_colors = ["#47cacc", "#cd83d4", "#e787c8", "#ffbe88", "#63bcc9",]
+
+# Generating the outside donut
 outside_donut = plt.pie(sizes,
-                        labes=labels,
+                        labels=labels,
                         colors=colors,
                         startangle=90,
                         frame=True,
                         autopct="%.2f%%",
                         pctdistance=0.80)
 
+# Generating the insite donut
 inside_donut = plt.pie(subgroup_sizes,
                        labels=subgroup_labels,
+                       colors=subgroup_colors,
                        radius=0.7,
                        startangle=90,
                        labeldistance=0.5,
                        autopct="%.2f%%",
                        pctdistance=0.35)
 
+# Creating a white circle for the middle which is what will make it look like a donut
 center_circle = plt.Circle((0,0), 0.35, color="white", linewidth=0)
 fig = plt.gcf()
 fig.gca().add_artist(center_circle)
 plt.axis("equal")
 plt.tight_layout
 
-# Shows the graph
+# Shows the graphs
 plt.show()
