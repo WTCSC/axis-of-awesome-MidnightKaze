dec6cf9891e8f7c3cae3fc6cb8f979509fb193be
Kat <cpnguyen0000@gmail.com>
2025-03-27 12:46:43 -0600

    Finished one of the two visuals, and started the read me

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=18859027)
 # Axis of Awesome
 
-ignore
-Totals,451,302,8,33,21,87
-, "Ledgendary", "Mythical", "Megas"
-, df.Ledgendary, df.Mythical, df.Megas
+Are you a complete Pokemon ~~addict~~ fan like me? Well maybe you're curious in seeing how all of the types have a rather unfair distribution of each "type" of Pokemon. I'm talking about the fact that psychic type pokemon have __ten__ mega evolutions compared to the __two__ in eletric type. That's just a little unfair. 
+
+And listen, only a nerd would care about that, but it's kind of cool. And instead of a boring old data table, you can look at some awesome visualizations!
+
+## How to Visualize
--- visuals.py
+++ visuals.py
@@ -2,7 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-columns = ["Type", "Total", "Regular"]
+# Starts by pulling the data from the csv file
+columns = ["Type", "Total", "Regular", "PseudoLedgendary", "Ledgendary", "Mythical", "Megas"]
 df = pd.read_csv("Axis of Awesome Data - Sheet1.csv", usecols=columns)
-plt.plot(df.Type, df.Total, df.Regular)
-plt.show()
+
+# Basic scatterplot visualization for the data set
+plt.scatter(df.Type, df.Total, color="red", label="Total")
+plt.scatter(df.Type, df.Regular, color="orange", label="Regular")
+plt.scatter(df.Type, df.PseudoLedgendary, color="green", label="Pseudo Ledgendary")
+plt.scatter(df.Type, df.Ledgendary, color="blue", label="Ledgendary")
+plt.scatter(df.Type, df.Mythical, color="purple", label="Mythical")
+plt.scatter(df.Type, df.Megas, color="pink", label="Mega")
+
+# Defines the axis labels
+plt.xlabel('Type')
+plt.ylabel('Number of Pokemon')
+plt.legend() # Generates the legend
+plt.show() # Shows the graph
